17bf8f515aca0f24ae919ec517641db51df6e944
Kyu Yeun Kim <kyuyeunk01@gmail.com>
2020-08-15 12:10:15 +0900

    Implemented lunar lander for A2C

---
+++ A2C/lunar_lander.py
@@ -0,0 +1,27 @@
+import torch
+import numpy
+from shared.gym_env import Environment
+from A2C.a2c import a2c, HyperParameters
+
+
+def main():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    env = Environment('LunarLander-v2', device)
+    n_inputs = numpy.prod(env.get_shape_observations())
+    n_outputs = env.get_n_actions()
+
+    # Hyper parameters
+    hyper_parameters = {
+        HyperParameters.TRAIN_EPISODES: 2000,
+        HyperParameters.ACTOR_LEARNING_RATE: 0.00001,
+        HyperParameters.CRITIC_LEARNING_RATE: 0.0002,
+        HyperParameters.GAMMA: 0.99,
+        HyperParameters.ACTOR_LAYERS: [n_inputs, 256, 256, 128, n_outputs],
+        HyperParameters.CRITIC_LAYERS: [n_inputs, 256, 256, 128, 1]
+    }
+
+    a2c(env, hyper_parameters)
+
+
+if __name__ == "__main__":
+    main()
